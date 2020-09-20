349ce1c2c34c8ec9ef6fc02164ffff218ca3b1ac
KC <fbiito2@gmail.com>
2020-09-20 09:14:58 +0800

    V1

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private NorthwindContext _db = new NorthwindContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);    
+        }
+        public ActionResult Index()
+        {
+            return View(_db.Employees.ToList());
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employees _employee)
+        {
+            _db.Employees.Add(_employee);
+            _db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
